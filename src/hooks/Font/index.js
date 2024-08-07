953ac7b0e27ecb3c240989d2eeb54e0991c027b0
Felipewxyz <159555476+Felipewxyz@users.noreply.github.com>
2024-08-07 14:55:59 +0000

    [feat] Expo-font with Context

---
+++ src/hooks/Font/index.js
@@ -0,0 +1,34 @@
+import { useFonts } from "expo-font";
+import { createContext, useContext } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+
+const FontContext = createContext({})
+
+export function FontProvider({ children }) {
+    const [loaded, error] = useFonts({
+        light: require("../../assets/fonts/JosefinSans-Light.ttf"),
+        lightitalic: require("../../assets/fonts/JosefinSans-LightItalic.ttf"),
+        regular: require("../../assets/fonts/JosefinSans-Regular.ttf"),
+        medium: require("../../assets/fonts/JosefinSans-Medium.ttf"),
+        semibold: require("../../assets/fonts/JosefinSans-SemiBold.ttf"),
+    });
+
+    if (!loaded && !error) {
+       return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+            <Text style={{ fontSize: 28, marginTop: 15 }}>Carregando as Fontes</Text>
+            <ActivityIndicator size="small" color="#6A5ACD"/>
+        </View>
+        )
+    }
+
+    return <FontContext.Provider value={{}}>{children}</FontContext.Provider>
+}
+
+export function useFont() {
+    const context = useContext(FontContext)
+    if (!context) {
+        throw new Error("useFont must be used within a FontProvider");
+    }
+    return context;
+}
