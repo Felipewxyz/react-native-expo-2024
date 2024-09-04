a21d19f21cee1fa9d9c4776fd47f1915f034df9f
Felipewxyz <159555476+Felipewxyz@users.noreply.github.com>
2024-09-04 11:56:27 +0000

    [feat] PagerView in the Home

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -4,33 +4,32 @@ import { useAuth } from "../hooks/Auth";
 import { useEffect } from "react";
 
 const StackLayout = () => {
-    const { user } = useAuth();
-    const segments = useSegments();
+  const { user } = useAuth();
+  const segments = useSegments();
 
-    useEffect(() => {
-        const inAuthGroup = segments[0] === "(protected)";
+  useEffect(() => {
+    const inAuthGroup = segments[0] === "(protected)";
 
-        if (!user?.autenticated && inAuthGroup) {
-            router.replace("/");
-        }   else {
-            if (user?.autenticated) {
-                router.replace("/(protected)");
-            }
-        }
-    }, [user]);
+    if (!user?.autenticated && inAuthGroup) {
+      router.replace("/");
+    } else {
+      if (user?.autenticated) {
+        router.replace("/(protected)");
+      }
+    }
+  }, [user]);
 
-    return (
-        <Stack>
-            <Stack.Screen name="index" options={{ headerShown: false }}/>
-            <Stack.Screen name="(protected)" options={{ headerShown: false }}/>
-        </Stack>
-    );
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(protected)" options={{ headerShown: false }} />
+    </Stack>
+  );
 };
 export default function Layout() {
-
-    return (
-        <AppProvider>
-            <StackLayout />
-        </AppProvider>
-    );
+  return (
+    <AppProvider>
+      <StackLayout />
+    </AppProvider>
+  );
 }
