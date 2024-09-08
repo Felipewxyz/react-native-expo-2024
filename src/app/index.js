6626092355bbb35eb12ce9cf5ca7779384af4fcc
Felipewxyz <159555476+Felipewxyz@users.noreply.github.com>
2024-09-08 20:39:27 +0000

    [fix] Create Payments Datatable

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { router } from "expo-router";
-import { Alert, BackHandler, Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Alert,
+  BackHandler,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { useAuth } from "../hooks/Auth";
 import { Ionicons } from "@expo/vector-icons";
 import { useState } from "react";
@@ -13,7 +21,7 @@ export default function App() {
 
   const tooglePasswordVisibility = () => {
     setPasswordVisibility(!passwordVisibility);
-  }
+  };
 
   const handleEntrarSuper = async () => {
     try {
@@ -30,34 +38,34 @@ export default function App() {
       <Text style={styles.title}>Aplicativo Pronto para Usar</Text>
       <View style={styles.inputbox}>
         <Ionicons name="mail-open-outline" size={20} color="black" />
-        <TextInput 
-          style={styles.emailinput} 
-          placeholder="E-mail" 
-          value={email} 
+        <TextInput
+          style={styles.emailinput}
+          placeholder="E-mail"
+          value={email}
           onChangeText={setEmail}
         />
       </View>
       <View style={styles.inputbox}>
         <Ionicons name="lock-closed-outline" size={20} color="black" />
-        <TextInput 
-          style={styles.emailinput} 
-          placeholder="Senha" 
-          value={password} 
+        <TextInput
+          style={styles.emailinput}
+          placeholder="Senha"
+          value={password}
           onChangeText={setPassword}
           secureTextEntry={passwordVisibility}
         />
         <Ionicons
-         name={passwordVisibility ? "eye-off-outline" : "eye-outline"}
-         size={20} 
-         color="black" 
-         onPress={tooglePasswordVisibility}
+          name={passwordVisibility ? "eye-off-outline" : "eye-outline"}
+          size={20}
+          color="black"
+          onPress={tooglePasswordVisibility}
         />
       </View>
 
       <Button
-       style={styles.button} 
-       title="Entrar" 
-       onPress={handleEntrarSuper} 
+        style={styles.button}
+        title="Entrar"
+        onPress={handleEntrarSuper}
       />
       <Button title="Sobre" onPress={() => router.push("/about")} />
       <Button
@@ -72,15 +80,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     gap: 15,
   },
 
   title: {
     fontFamily: "regular",
-    fontSize: 20, 
+    fontSize: 20,
   },
   inputbox: {
     flexDirection: "row",
@@ -97,5 +105,4 @@ const styles = StyleSheet.create({
   button: {
     width: "100%",
   },
-
 });
