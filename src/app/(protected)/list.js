df83eb581b8888bcf2fb4f286b079c08454d2ad1
Felipewxyz <159555476+Felipewxyz@users.noreply.github.com>
2024-11-24 19:58:37 +0000

    [fix] FlashList Payments

--- src/app/(protected)/list.js
+++ src/app/(protected)/list.js
@@ -8,42 +8,69 @@ import { formatCurrencyBRL } from "../../utils/formatCurrent";
 export default function List() {
   const [data, setData] = useState([]);
   const { getPayments } = usePaymentsDatabase();
+  const [page, setPage] = useState(0); //controlar qual pág o sistema já carregou
+  const [loading, setLoading] = useState(true); //controlar se está carregando os dados do banco
+  const [hasMore, setHasMore] = useState(true); //controlar se tem mais dados para carregar
 
-  async function fetchData(params) {
+  async function fetchData() {
+    if ((hasMore = false)) return; //Se esta flag for falsa não tem mais dados para carregar
+    console.log(page);
+    setPage(page + 1);
     //Vai buscar no banco de dados os pagamentos
-    const payments = await getPayments();
-    console.log(payments);
-    setData(payments);
+    const payments = await getPayments(page);
+
+    if (payments.length < 5) setHasMore(false); //Se retornar menos de 5 registros, não tem mais dados para carregar
+    //console.log(payments);
+    setData([...data, ...payments]);
+    setLoading(false);
   }
 
   useEffect(() => {
     //Executa a primeira fez banco de dados
-    // console.log("passou pelo useEffect")
+    setPage(0);
     fetchData();
   }, []);
 
   renderItem = ({ item }) => {
-    <View style={{flexDirection: "row", margin: 5}}>
-      <View>
-        <Text>{item.nome}</Text>
+    <View
+      style={{
+        flexDirection: "row",
+        margin: 5,
+        margin: 10,
+        padding: 3,
+        backgroundColor: "#fff",
+      }}
+    >
+      <View style={{ flex: 1, gap: 5 }}>
+        <Text style={{ fontFamily: "regular", fontSize: 18, textTransform: "uppercase" }}>{item.nome}</Text>
       </View>
-      <View style={{flexDirection: "row", justifyContent: "space-between"}}>
-      <Text>{formatDateToBrazilian(item.data_pagamento || new Date())}</Text>
-      <Text>{item.numero_recibo}</Text>
+      <View style={{ flexDirection: "row", gap: 10 }}>
+        <Text style={{ fontFamily: "regular" }}>
+          {formatDateToBrazilian(item.data_pagamento || new Date())}
+        </Text>
+        <Text>{item.numero_recibo}</Text>
       </View>
       <View>
-        <Text>{formatCurrencyBRL(item.valor_pago || 0)}</Text>
+        <Text
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          {formatCurrencyBRL(item.valor_pago || 0)}
+        </Text>
       </View>
     </View>;
   };
 
   return (
     <View style={{ flex: 1 }}>
+      <Text>Pagamentos</Text>
       <View style={{ flex: 1 }}>
         <FlashList
           data={data}
           renderItem={renderItem}
-          estimatedItemSize={200}
+          estimatedItemSize={50}
+          onEndReached={fetchData}
+          onEndReachedThreshold={0.8}
+          keyExtractor={(item) => item.id.toString()}
         />
       </View>
     </View>
